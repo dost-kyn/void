dccafe69bbb7d922cd828c8119307d840d961c3e
еленя <plishivui@gmail.com>
2025-11-07 00:15:45 +0400

    Resolve merge conflict in App.jsx

--- void_react/src/App.jsx
+++ void_react/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-
+import {  Route, Routes } from 'react-router-dom'
 import './App.css'
 
 import Posts from './pages/Posts'
-import {  Route, Routes } from 'react-router-dom'
+import Authorization from './pages/Authorization'
+
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route  path='/posts' element={<Posts/>}/>
+        <Route  path='/' element={<Posts/>}/>
+        <Route  path='/authorization' element={<Authorization/>}/>
       </Routes>
     </>
   )
