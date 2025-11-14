c4a6b3ab5ca6520a385cd37bd04a976a6f884ed6
dost-kyn <dostoevskijf700@gmail.com>
2025-11-14 21:19:17 +0400

    api

--- void_express/features/users/user.controller.js
+++ void_express/features/users/user.controller.js
@@ -1,81 +1,103 @@
 const { message } = require("../../utils/configuration.prisma");
 const UserService = require("./user.service");
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.getAllUsers = async (req, res, next) => {
-  const users = await UserService.getAllUsers();
-  if (users.length <= 0) {
-    const error = new Error("Пользователи не найдены");
-    error.status = 404;
-    return next(error);
-  }
-  res.status(200).json(users);
+    const users = await UserService.getAllUsers();
+    if (users.length <= 0) {
+        const error = new Error("Пользователи не найдены");
+        error.status = 404;
+        return next(error);
+    }
+    res.status(200).json(users);
 };
 
+
+
+
+
 exports.createUser = async (req, res, next) => {
-    const {name, last_name, login, email, password, repeatPassword, avatar} = req.body;
-    const VerifyCreateUser = await UserService.VerifyCreateUser(name, last_name, login, email, password, repeatPassword)
-    // if(VerifyCreateUser){
-        const VerifyPasswords = await UserService.VerifyPasswords(password, repeatPassword)
-        const GetUsersByEmail = await UserService.GetUsersByEmail(email)
-        const GetUsersByLogin = await UserService.GetUsersByLogin(login)
+    const { name, last_name, login, email, password, repeatPassword, avatar } = req.body;
 
-        if(VerifyCreateUser){
-            return res.status(400).json({message: VerifyCreateUser})
-        }
-        if(VerifyPasswords){
-            return res.status(400).json({message: VerifyPasswords})
-        }
-        if(GetUsersByEmail){
-            return res.status(400).json({message: GetUsersByEmail})
-        }
-        if(GetUsersByLogin){
-            return res.status(400).json({message: GetUsersByLogin})
-        }
+    const VerifyCreateUser = await UserService.VerifyCreateUser(
+        name,
+        last_name,
+        login,
+        email,
+        password,
+        repeatPassword
+    );
 
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = await UserService.createUser(userData)
+    const VerifyPasswords = await UserService.VerifyPasswords(
+        password,
+        repeatPassword
+    );
+    const GetUsersByEmail = await UserService.GetUsersByEmail(email);
+    const GetUsersByLogin = await UserService.GetUsersByLogin(login);
 
-        const token = jwt.sign({
+    if (VerifyCreateUser) return res.status(400).json({ message: VerifyCreateUser });
+    if (VerifyPasswords) return res.status(400).json({ message: VerifyPasswords });
+    if (GetUsersByEmail) return res.status(400).json({ message: GetUsersByEmail });
+    if (GetUsersByLogin) return res.status(400).json({ message: GetUsersByLogin });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await UserService.createUser({
+        name, last_name, login, email, hashedPassword, avatar
+    });
+
+    const token = jwt.sign(
+        {
             id: newUser.id,
-            role: newUser.role
-        }, process.env.JWT_SECRET, {
-            expireIn: '5h'
-        })
-
-        res.status(200).json({message: 'Пользователь зарегистрирован', token})
-    // }
-}
-
-
-
-
-
-
-exports.loginUsers = async(req, res, next) => {
-    const {login, password} = req.body
-    const passwordNoHash = bcrypt.hashSync(password)// - првоерить
-
-    const VerifyAuto = await UserService.VerifyAuto(login, password)
-    // if(VerifyAuto){
-        const user = await UserService.GetUsersByLogin(login)
-        const AutoPasswords = await UserService.AutoPasswords(user, passwordNoHash)   
-
-        if(user){
-            res.status(400).json({message: user})
+            role: newUser.role,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "5h",
         }
-        if(AutoPasswords){
-            res.status(400).json({message: AutoPasswords})
+    );
+
+    res.status(200).json({ message: "Пользователь зарегистрирован", token });
+
+};
+
+
+
+
+
+exports.loginUsers = async (req, res, next) => {
+    const { login, password } = req.body;
+
+
+    // const passwordNoHash = bcrypt.hashSync(password); // - првоерить
+
+    const VerifyAuto = await UserService.VerifyAuto(login, password);
+    if (VerifyAuto) return res.status(400).json({ message: VerifyAuto });
+
+    const user = await UserService.GetUsersByLogin(login); // Находим массив пользователя по логину
+    const AutoPasswords = await UserService.AutoPasswords(user, password); // Передаем user и обычный пароль
+
+
+    if (loginCheck) {
+        return res.status(400).json({ message: loginCheck });
+    }
+    if (AutoPasswords) {
+        res.status(400).json({ message: AutoPasswords });
+    }
+
+    const token = jwt.sign(
+        {
+            id: user.id,
+            role: user.role
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "5h"
         }
+    );
 
-        const token = jwt.sign({
-            id: newUser.id,
-            role: newUser.role
-        }, process.env.JWT_SECRET, {
-            expireIn: '5h'
-        })
+    res.status(200).json({ message: "Пользователь Вошли", token });
+};
 
-        res.status(200).json({message: 'Пользователь Вошли', token})
-    // }
-}
+
+
---
+++ void_react/src/api/users.api.js
@@ -0,0 +1,20 @@
+const API_URL = 'http://localhost:5000/api';
+
+// Регистрация
+export const registerUser = async (userData) => {
+    const response = await fetch(`${API_URL}/users/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+    });
+    return await response.json();
+};
+
+
+// Получить всех пользователей
+export const getAllUsers = async() => {
+    const response = await fetch(`${API_URL}/users/`)
+    return await response.json();
+}
